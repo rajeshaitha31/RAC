df777d1e61b3b362ae0bafa21dc51123f49edf2f
Rajesh <rajesh.aitha@cigniti.com>
2021-04-18 12:19:49 +0530

    commit

--- Grid/StartHub.bat
+++
@@ -1,4 +0,0 @@
-cd..
-cd %workspace%\Grid
-java -jar selenium-server-standalone-2.53.1.jar -port 4444 -role hub -browserTimeout 900000
-pause
--- Grid/StartNodes.bat
+++
@@ -1,4 +0,0 @@
-cd..
-cd %workspace%\Grid
-java -jar selenium-server-standalone-2.53.1.jar -role node -nodeConfig selenium-node-win.cfg.json
-pause
